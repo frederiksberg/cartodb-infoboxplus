7b48bfc426ad2b44713279829e4c52cd09b5ef77
Niels Kjøller Hansen <niels.k.h@gmail.com>
2016-10-26 09:51:21 +0200

    Clean dist directory

--- dist/components/models.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var InfoModel = (function () {
-    function InfoModel(controller) {
-        this.controller = controller;
-    }
-    return InfoModel;
-}());
-exports.InfoModel = InfoModel;
-//# sourceMappingURL=models.js.map
